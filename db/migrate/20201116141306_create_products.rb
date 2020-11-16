36ae71d6defea1ce40ab430536fe893f934f750d
Hugo Le Morvan <lm.hugo@hotmail.fr>
2020-11-16 15:15:49 +0100

    Model Product Booking créé

---
+++ db/migrate/20201116141306_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.float :latitude
+      t.float :longitude
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
